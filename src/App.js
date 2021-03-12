5251d3099c6a361fc5bd6d72e4df3db5290b3331
doanhnguyen99 <doanh080199@gmail.com>
2021-03-12 15:26:40 +0700

    TodoList Item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import './App.scss';
-import ColorBox from './component/ColorBox';
+import TodoList from './component/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+      {id: 1, title: 'front-end'},
+      {id: 2, title: 'demo front-end'},
+      {id: 3, title: 'demo react front-end'},
+    ]);
+  
+  function handleTodoClick(todo){
+    console.log(todo);
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if(index < 0) return;
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>Welcome to React Hooks</h1>
-      <ColorBox />
+      <h1>React hook - TodoList</h1>
+      {/* <ColorBox /> */}
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
